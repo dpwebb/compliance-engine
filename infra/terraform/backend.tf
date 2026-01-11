34dad2955554f401614e0688292f6255f68fbf49
David Webb <davidphilipwebb@gmail.com>
2026-01-11 17:53:01 -0400

    infra: s3 backend via workspaces (no vars in backend)

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "ce-tfstate-460425809402"
-    key            = "stacks/${var.environment}/terraform.tfstate"
-    region         = "ca-central-1"
-    dynamodb_table = "ce-tf-lock-460425809402"
-    encrypt        = true
+    bucket               = "ce-tfstate-460425809402"
+    key                  = "stacks/terraform.tfstate"
+    region               = "ca-central-1"
+    dynamodb_table       = "ce-tf-lock-460425809402"
+    encrypt              = true
+    workspace_key_prefix = "env"
   }
 }
